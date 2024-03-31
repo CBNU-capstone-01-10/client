59fd3eee88bc157c5e30a9c7ade55986eb0e01aa
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-03-31 11:09:10 +0900

    feat: 사용자 위치 감지 추가

--- src/(routes)/record/page.tsx
+++ src/(routes)/record/page.tsx
@@ -8,6 +8,9 @@ interface IUseInterval {
 export default function Page() {
   const socket = io();
   const [stream, setStream] = useState<MediaStream>();
+  const [geoLocPosition, setGeoLocPosition] = useState<number[]>([0, 0]);
+  const [geoLocWatchId, setGeoLocWatchId] = useState();
+
   const videoRef = useRef<HTMLVideoElement>(null);
 
   const getCameraPermission = async () => {
@@ -23,16 +26,77 @@ export default function Page() {
     }
   };
 
+  const geoOptions = {
+    // enableHighAccuracy: false,
+    // maximumAge: 0,
+    timeout: 15000,
+  };
+
+  const markPosition = (pos: GeolocationPosition) => {
+    const latitude = pos.coords.latitude;
+    const longitude = pos.coords.longitude;
+
+    if (latitude !== null && longitude !== null) {
+      setGeoLocPosition([latitude, longitude]);
+    }
+
+    console.log(latitude, longitude);
+  };
+
+  const showErrorMsg = (error: GeolocationPositionError) => {
+    console.log(error.code);
+    console.log(error.PERMISSION_DENIED);
+    try {
+      if (error.code === error.PERMISSION_DENIED) {
+        console.log("요청거부");
+      }
+      if (error.code === error.POSITION_UNAVAILABLE) {
+        console.log("위치정보 사용불가");
+      }
+      if (error.code === error.TIMEOUT) {
+        console.log("요청시간초과");
+      }
+    } catch (e) {
+      console.log("알 수 없는 에러");
+    } finally {
+      console.log("위치정보 수집에 따른 부가적인 알림을 확인할 수 없습니다.");
+    }
+  };
+
+  const getGPSPermission = async () => {
+    try {
+      const id = navigator.geolocation.watchPosition(
+        // getCurrentPosition 버전
+        // navigator.geolocation.getCurrentPosition(
+        markPosition,
+        showErrorMsg,
+        geoOptions
+      );
+      console.log(id);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    console.log("geo");
+    console.log(geoLocPosition);
+  }, [geoLocPosition]);
+
   const drawVideoSnapshot = (videoElement: HTMLVideoElement) => {
     const video = videoElement;
+
     const canvas = document.createElement("canvas");
 
+    const dpr = window.devicePixelRatio;
+
     const ctx = canvas.getContext("2d");
-    // ctx?.scale(dpr, dpr);
+    ctx?.scale(dpr, dpr);
 
     if (video) {
       ctx?.drawImage(video, 0, 0, canvas.width, canvas.height);
       const imageData = canvas.toDataURL("image/jpeg");
+      // console.log(imageData);
 
       // 이미지 전송
       socket.emit("image", imageData);
@@ -62,7 +126,14 @@ export default function Page() {
     if (videoRef.current) {
       drawVideoSnapshot(videoRef.current);
     }
-  }, 5000);
+  }, 2000);
+
+  // getCurrentPosition 버전
+  // useInterval(() => {
+  //   if (geoLocPosition) {
+  //     markPosition(geoLocPosition);
+  //   }
+  // }, 2000);
 
   useEffect(() => {
     if (!videoRef.current) {
@@ -75,6 +146,7 @@ export default function Page() {
 
   useEffect(() => {
     getCameraPermission();
+    getGPSPermission();
 
     return () => {
       socket.disconnect();
@@ -88,7 +160,7 @@ export default function Page() {
         ref={videoRef}
         id="local-video"
         width="375"
-        height=""
+        height="280"
         autoPlay
         muted
         loop
