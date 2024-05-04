b3275ae86cc8ff0a11b391b9c61d3f48ec928981
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-05-04 20:02:16 +0900

    chore: css 공통 테마 추가

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,9 @@ import { theme } from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    {/* <ThemeProvider theme={theme}> */}
+    <GlobalStyle />
+    <App />
+    {/* </ThemeProvider> */}
   </React.StrictMode>
 );
