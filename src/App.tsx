a830882b6eab10da84e9ff6b94ba62ce742f1769
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-26 01:37:43 +0900

    feat: App react-query provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-// import { useState } from "react";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import router from "./router";
+import axios from "axios";
 
-function App() {
-  return <RouterProvider router={router} />;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default function App() {
+  axios.defaults.withCredentials = true;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
-
-export default App;
