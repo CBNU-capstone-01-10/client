cb468adae638d5bcf882f8d76bb4eaa63c1ac38c
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-27 00:35:47 +0900

    feat: 실시간 점수 로그 수정

--- src/(routes)/record/_components/LiveScoreLog.tsx
+++ src/(routes)/record/_components/LiveScoreLog.tsx
@@ -1,22 +1,16 @@
-import {
-  LiveScoreLogWrapper,
-  LiveScoreLogItem,
-  ContentWrapper,
-  Content,
-  ElapsedTime,
-  Score,
-} from "../style/style";
+import ContentBlockWrapper from "../../../_components/block/content-block-wrapper";
+import * as S from "../style/LiveScoreLog.style";
 
-const alert = [
+const liveScoreLog = [
   {
     text: "안전운전 20분 유지",
     elapsedTime: "2분 17초 전",
     score: 8.0,
   },
   {
-    text: "안전운전 10분 유지",
-    elapsedTime: "2분 17초 전",
-    score: 8.0,
+    text: "전방주시 태만",
+    elapsedTime: "3분 17초 전",
+    score: -20.0,
   },
   {
     text: "안전운전 10분 유지",
@@ -46,18 +40,20 @@ const alert = [
 ];
 export default function LiveScoreLog() {
   return (
-    <>
-      <LiveScoreLogWrapper>
-        {alert.map((alertItem, idx) => (
-          <LiveScoreLogItem key={idx}>
-            <ContentWrapper>
-              <Content>{alertItem.text}</Content>
-              <ElapsedTime>{alertItem.elapsedTime}</ElapsedTime>
-            </ContentWrapper>
-            <Score>{alertItem.score}</Score>
-          </LiveScoreLogItem>
-        ))}
-      </LiveScoreLogWrapper>
-    </>
+    <S.LiveScoreLogWrapper>
+      {liveScoreLog?.map((liveScoreLogItem, idx) => (
+        <ContentBlockWrapper key={idx}>
+          <S.LiveScoreLogItem>
+            <S.ContentWrapper>
+              <S.Content>{liveScoreLogItem.text}</S.Content>
+              <S.ElapsedTime>{liveScoreLogItem.elapsedTime}</S.ElapsedTime>
+            </S.ContentWrapper>
+            <S.ScoreWrapper score={liveScoreLogItem.score}>
+              {liveScoreLogItem.score}
+            </S.ScoreWrapper>
+          </S.LiveScoreLogItem>
+        </ContentBlockWrapper>
+      ))}
+    </S.LiveScoreLogWrapper>
   );
 }
