798a7e8d83e815643a243e293ad2cd6a80561b93
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-27 00:29:52 +0900

    design: 녹화화면 디자인 수정

--- src/(routes)/record/page.style.ts
+++ src/(routes)/record/page.style.ts
@@ -2,12 +2,21 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   width: 100%;
-  padding-top: var(--top-navbar-height);
+  height: 100vh;
+  padding: calc(var(--top-navbar-height) - 2rem) 1rem 0 1rem;
+  background-color: ${({ theme }) => theme.primaryBgColor};
+`;
+export const ContentWrapper = styled.div`
+  width: 100%;
+  height: 100%;
 `;
 export const VideoWrapper = styled.div`
   width: 100%;
+  height: fit-content;
+  margin: 0.8rem 0;
 `;
 export const VideoElement = styled.video`
+  border-radius: 1.5rem;
   width: 100%;
   display: block;
   margin: 0 auto;
