d4329a4c7c5428f1d96ab2e0c89e1091bf012d31
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-27 00:31:06 +0900

    feat: 마이페이지 내 로그아웃 버튼 추가

--- src/(routes)/profile/page.tsx
+++ src/(routes)/profile/page.tsx
@@ -1,7 +1,11 @@
+import * as S from "./page.style";
+import LogoutButton from "./_components/logout-button/logout-button";
+
+// PAGE: 프로필 페이지
 export default function Page() {
   return (
-    <>
-      <h1>Profile</h1>
-    </>
+    <S.Wrapper>
+      <LogoutButton>로그아웃</LogoutButton>
+    </S.Wrapper>
   );
 }
