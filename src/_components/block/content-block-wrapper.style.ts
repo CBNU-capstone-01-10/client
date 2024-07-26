f5c58515286ef2ae2ce3cec5821315af1d6283c4
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-26 15:16:36 +0900

    feat: 내용 블럭 래퍼 추가

---
+++ src/_components/block/content-block-wrapper.style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 90%;
+  height: 5rem;
+  background-color: white;
+  border-radius: 1.5rem;
+  box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;
+`;
---
+++ src/_components/block/content-block-wrapper.tsx
@@ -0,0 +1,5 @@
+import * as S from "./content-block-wrapper.style";
+
+export default function ContentBlockWrapper({ children }) {
+  return <S.Wrapper>{children}</S.Wrapper>;
+}
