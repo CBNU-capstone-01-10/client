e350ff5e8d9245ff2dc15cd95ea0a18f306bf0f3
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-27 00:31:56 +0900

    design: 마이페이지 디자인 추가

---
+++ src/(routes)/profile/page.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: var(--top-navbar-height) 1rem var(--btm-navbar-height) 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${({ theme }) => theme.primaryBgColor};
+`;
