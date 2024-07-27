e8c1761793b47dcab649d8cc954a7ade6e14e8b9
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-27 22:45:12 +0900

    feat: 회원가입 관련 타입 추가

--- src/(routes)/(account)/types/type.ts
+++ src/(routes)/(account)/types/type.ts
@@ -3,3 +3,21 @@ export interface ILoginParams {
   email: string;
   password: string;
 }
+
+// 회원가입시 입력값
+export interface IAccountRegistrationData {
+  username: string;
+  email: string;
+  password: string;
+}
+
+// 회원가입시 응답값
+export interface IRegisterResponseData {
+  created_at: string;
+  email: string;
+  encrypted_password: string;
+  id: number;
+  pfp: string;
+  updated_at: string;
+  username: string;
+}
