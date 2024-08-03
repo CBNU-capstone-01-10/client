e025bbd760ffbe4e960dc6a0b8942b30f93b08d3
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-08-03 14:16:36 +0900

    feat: 회원가입 이름 입력 스텝 추가

---
+++ src/(routes)/(account)/signup/components/account-setup-step/setup-username.tsx
@@ -0,0 +1,43 @@
+import { useFormContext } from "react-hook-form";
+import * as S from "./setup-step.style";
+import { ISignupParams } from "../../../types/type";
+
+// STEP-PAGE: 이름 입력
+type IFieldValues = Pick<ISignupParams, "username">;
+interface ISetupUsernameProps {
+  onNext: () => void;
+}
+export default function SetupUsername({ onNext }: ISetupUsernameProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useFormContext<IFieldValues>();
+
+  // HANDLER
+  const onSubmit = (data: IFieldValues) => {
+    if (data.username) {
+      onNext();
+    }
+  };
+
+  return (
+    <S.SetupPageWrapper>
+      <S.InputContainer>
+        <S.Input
+          type="text"
+          placeholder="이름을 입력하세요"
+          required
+          {...register("username", {
+            required: "이름을 입력하세요",
+            maxLength: { value: 12, message: "이름은 12자 이내여야 합니다" },
+          })}
+        />
+        {errors.username && (
+          <S.ErrorMessage>{errors.username.message}</S.ErrorMessage>
+        )}
+      </S.InputContainer>
+      <S.NextBtn onClick={handleSubmit(onSubmit)}>다음</S.NextBtn>
+    </S.SetupPageWrapper>
+  );
+}
