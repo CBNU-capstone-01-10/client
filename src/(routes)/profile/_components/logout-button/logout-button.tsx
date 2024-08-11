fc76157dc87177ff8512c875c2519f7c241f82ee
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-08-12 00:21:10 +0900

    fix: 에러 수정 'Cannot update a component while rendering a different component warning'

--- src/(routes)/profile/_components/logout-button/logout-button.tsx
+++ src/(routes)/profile/_components/logout-button/logout-button.tsx
@@ -2,20 +2,24 @@ import { useNavigate } from "react-router";
 import ContentBlockWrapper from "../../../../_components/block/content-block-wrapper";
 import * as S from "./logout-button.style";
 import { useLogout } from "../../../../api/account";
+import { useEffect } from "react";
 
 // COMPONENT: 로그아웃 버튼
 export default function LogoutButton() {
   const navigate = useNavigate();
   const { mutate: logout, isSuccess: isLogoutSuccess } = useLogout();
+
   // HANDLER: 로그아웃
   const handleLogout = async () => {
     logout();
   };
 
   // ROUTE: 로그아웃 성공
-  if (isLogoutSuccess) {
-    navigate("/signin");
-  }
+  useEffect(() => {
+    if (isLogoutSuccess) {
+      navigate("/signin");
+    }
+  }, [isLogoutSuccess, navigate]);
 
   return (
     <ContentBlockWrapper>
