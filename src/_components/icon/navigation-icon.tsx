3af6450f46af75b9c898e68d15bbfdfb966bc549
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-08-21 22:56:51 +0900

    rename: 파일명 변경

---
+++ src/_components/icon/navigation-icon.tsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+import { IIconProps } from "../../interface/IconInterface";
+import * as S from "./navigation-icon.style";
+
+// 네비게이션바 아이콘
+export interface INavIconProps extends IIconProps {
+  to: string;
+  text?: string;
+}
+export default function NavIcon({
+  icon: Icon,
+  size,
+  color,
+  to,
+  text,
+}: INavIconProps) {
+  return (
+    <NavLink to={to}>
+      <S.IconWrapper size={size} color={color}>
+        <Icon />
+        <S.IconText>{text}</S.IconText>
+      </S.IconWrapper>
+    </NavLink>
+  );
+}
