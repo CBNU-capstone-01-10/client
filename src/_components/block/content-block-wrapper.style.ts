cc6e8914693b8918c05d6b99e25e5196d2a39433
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-09-21 23:59:46 +0900

    design: 컨텐트블럭 래퍼 스타일 수정

--- src/_components/block/content-block-wrapper.style.ts
+++ src/_components/block/content-block-wrapper.style.ts
@@ -6,6 +6,7 @@ interface WrapperProps {
 export const Wrapper = styled.div<WrapperProps>`
   width: 100%;
   height: ${(props) => props.height || "5rem"};
+  margin-bottom: 1.2rem;
   background-color: white;
   border-radius: 1.5rem;
   box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;
