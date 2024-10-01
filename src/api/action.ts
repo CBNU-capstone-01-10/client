281f785dc7ca228f3c6688172e0599ab5b489729
wonza-hub <wonza8585@gmail.com>
2024-10-01 21:40:52 +0900

    feat: 운전자 행위 결과 다건 조회 rest api 함수 추가

--- src/api/action.ts
+++ src/api/action.ts
@@ -1,7 +1,11 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios, { AxiosError, AxiosResponse } from "axios";
 import isServerError from "../error/is-server-error";
-import { IDriverActionRequestBody } from "../(routes)/record/types/type";
+import { getFormattedDate } from "../_utils/date";
+import {
+  IDriverActionRequestBody,
+  IDriverActionResponse,
+} from "../(routes)/record/types/type";
 
 // POST: 운전자 행위 캡처 이미지 전송
 export const useSendDriverActionCapture = () => {
@@ -30,5 +34,24 @@ export const useSendDriverActionCapture = () => {
           break;
       }
     },
+  });
+};
+
+// GET: 운전자 행위 결과 다건 조회
+export const useGetRecentDriverActions = () => {
+  return useQuery<IDriverActionResponse[]>({
+    queryKey: ["recent-driver-actions"],
+    queryFn: async () => {
+      const date = getFormattedDate();
+      const minutes = 30;
+      const recentDriverActionsURL = `/api/actions?date_start=${date}&date_end=${date}&before_m=${minutes}`;
+
+      return await axios.get(recentDriverActionsURL).then((res) => {
+        return res.data.response;
+        // // JSON-SERVER
+        // return res.data;
+      });
+    },
+    retry: 0,
   });
 };
