84478ddbc7d5d679fea77987aa8f8b486c8aa114
wonza-hub <wonza8585@gmail.com>
2024-10-01 21:30:29 +0900

    feat: 운전자 행위 캡처 이미지 전송 rest api 함수 추가

---
+++ src/api/action.ts
@@ -0,0 +1,34 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import isServerError from "../error/is-server-error";
+import { IDriverActionRequestBody } from "../(routes)/record/types/type";
+
+// POST: 운전자 행위 캡처 이미지 전송
+export const useSendDriverActionCapture = () => {
+  //   const queryClient = useQueryClient();
+  return useMutation<AxiosResponse, AxiosError, IDriverActionRequestBody>({
+    mutationFn: async (driverActionData) => {
+      const driverActionURL = `api/action`;
+      return await axios.post(driverActionURL, driverActionData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    },
+    onSuccess: () => {},
+    onError: (err) => {
+      if (!isServerError(err)) {
+        alert("에러가 발생했습니다. 관리자에게 문의해주세요.");
+        return;
+      }
+      switch (err.response?.status) {
+        case 400:
+          alert(err.response?.data.message);
+          break;
+        default:
+          alert("잠시후 다시 시도해주세요.");
+          break;
+      }
+    },
+  });
+};
