e0fcf86384b4f7f84052085d02d62049fa840adc
wonza-hub <wonza8585@gmail.com>
2024-10-06 01:12:41 +0900

    design: 안전점수 통계 페이지 디자인 추가

---
+++ src/(routes)/statistics/page.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const PageWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: calc(var(--top-navbar-height) - 2rem) 1rem 0 1rem;
+  background-color: ${({ theme }) => theme.primaryBgColor};
+`;
+export const ContentWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
