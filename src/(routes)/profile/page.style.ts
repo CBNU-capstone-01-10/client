44eceaba03b692db58acaa88c45daf9cac7a0d8d
wonza-hub <wonza8585@gmail.com>
2024-10-06 21:05:19 +0900

    design: 프로필 페이지디자인 수정

--- src/(routes)/profile/page.style.ts
+++ src/(routes)/profile/page.style.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const PageWrapper = styled.div`
   width: 100%;
   height: 100vh;
-  padding: var(--top-navbar-height) 1rem var(--btm-navbar-height) 1rem;
+  padding: calc(var(--top-navbar-height) - 2rem) 1rem var(--btm-navbar-height)
+    1rem;
   display: flex;
   flex-direction: column;
   align-items: center;
