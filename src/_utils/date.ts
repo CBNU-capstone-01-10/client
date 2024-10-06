73d0c2ad989cf22703dce9af64253d0e72d33bb2
wonza-hub <wonza8585@gmail.com>
2024-10-06 10:43:41 +0900

    refactor: 날짜를 포맷변환 함수에 dayjs 적용

--- src/_utils/date.ts
+++ src/_utils/date.ts
@@ -1,9 +1,10 @@
-// 오늘의 날짜를 'YYYY-MM-DD' 형식으로 변환하는 함수
-export const getFormattedDate = (): string => {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, "0");
-  const day = String(today.getDate()).padStart(2, "0");
+import dayjs from "dayjs";
 
-  return `${year}-${month}-${day}`;
+/**
+ * 날짜를 YYYY-MM-DD 형식으로 변환하는 함수
+ * @param dateString
+ * @returns "YYYY-MM-DD"
+ */
+export const getFormattedDate = (dateString: string): string => {
+  return dayjs(dateString).format("YYYY-MM-DD");
 };
