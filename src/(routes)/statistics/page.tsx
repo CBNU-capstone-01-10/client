30bff0108211a7908fbfdcf379c9fcdd8490b1ff
wonza-hub <wonza8585@gmail.com>
2024-10-06 11:54:42 +0900

    comment: 페이지 설명 주석 추가

--- src/(routes)/statistics/page.tsx
+++ src/(routes)/statistics/page.tsx
@@ -3,6 +3,7 @@ import RecentHistory from "./_components/recent-history/recent-history";
 import * as S from "./page.style";
 import RecentSevenDaysScore from "./_components/recent-seven-days-score/recent-seven-days-score";
 
+// PAGE: 안전점수 관련 통계 페이지
 export default function Page() {
   return (
     <S.PageWrapper>
