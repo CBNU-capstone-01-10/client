7fe21f802b275dfe037473d4023e24594eb34f1b
wonza-hub <wonza8585@gmail.com>
2024-10-06 13:51:00 +0900

    feat: 프로필 페이지 레이아웃 수정

--- src/(routes)/profile/page.tsx
+++ src/(routes)/profile/page.tsx
@@ -1,11 +1,14 @@
 import * as S from "./page.style";
 import LogoutButton from "./_components/logout-button/logout-button";
+import PersonalInfo from "./_components/personal-info/personal-info";
+import ContentBlockWrapper from "../../_components/block/content-block-wrapper";
 
 // PAGE: 프로필 페이지
 export default function Page() {
   return (
     <S.Wrapper>
-      <LogoutButton>로그아웃</LogoutButton>
+      <PersonalInfo />
+      <LogoutButton />
     </S.Wrapper>
   );
 }
