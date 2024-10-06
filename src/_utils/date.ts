35c80df0bbc75d0a1865b7ed1fce86a57456cb27
wonza-hub <wonza8585@gmail.com>
2024-10-06 11:54:21 +0900

    feat: 날짜 포맷 형식 수정

--- src/_utils/date.ts
+++ src/_utils/date.ts
@@ -1,10 +1,10 @@
 import dayjs from "dayjs";
 
 /**
- * 날짜를 YYYY-MM-DD 형식으로 변환하는 함수
+ * 날짜를 YYYY-MM-DD HH:mm 형식으로 변환하는 함수
  * @param dateString
- * @returns "YYYY-MM-DD"
+ * @returns "YYYY-MM-DD HH:mm"
  */
 export const getFormattedDate = (dateString: string): string => {
-  return dayjs(dateString).format("YYYY-MM-DD");
+  return dayjs(dateString).format("YYYY/MM/DD HH:mm");
 };
