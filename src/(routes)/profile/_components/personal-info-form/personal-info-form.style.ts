4bd5a8b8d5bb272744bb1bf274e6437823e068ac
wonza-hub <wonza8585@gmail.com>
2024-10-09 22:52:20 +0900

    design: 사용자 정보 수정 폼 디자인 추가

---
+++ src/(routes)/profile/_components/personal-info-form/personal-info-form.style.ts
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+
+export const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
+export const Form = styled.form`
+  /* width: 100%; */
+`;
+export const PersonalInfoWrapper = styled.div`
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  gap: 0.5rem;
+`;
+export const PfpWrapper = styled.div`
+  position: relative;
+  width: 5rem;
+  height: 5rem;
+`;
+export const PfpImgWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 0.6rem;
+  overflow: hidden;
+`;
+export const Pfp = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+export const FileInput = styled.input`
+  visibility: hidden;
+  display: none;
+`;
+export const HiddenUploadBtn = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  color: black;
+`;
+export const FileDeleteBtn = styled.button`
+  width: 3.6rem;
+  height: 2.2rem;
+  background-color: #f0f3fa;
+  border-radius: 0.6rem;
+  font-weight: 900;
+  color: red;
+  float: right;
+`;
+export const Input = styled.input`
+  outline-color: ${({ theme }) => theme.primaryColor};
+  border: 1px solid black;
+  border-radius: 0.8rem;
+  padding: 0 1rem;
+  outline: none;
+  width: 100%;
+  height: 2rem;
+  margin: 0.2rem 0;
+`;
+export const Label = styled.label`
+  color: black;
+  font-size: 0.9rem;
+  margin: 0.3rem 0.5rem;
+`;
+export const UserName = styled.div`
+  color: black;
+  font-weight: 600;
+  width: 100%;
+`;
+export const UserAlias = styled.div`
+  color: black;
+  font-weight: 600;
+  width: 100%;
+`;
+export const UserAddress = styled.div`
+  color: black;
+  font-weight: 600;
+  width: 100%;
+`;
+export const CompleteButton = styled.button`
+  width: 3.6rem;
+  margin-left: auto;
+  height: 2.2rem;
+  background-color: #f0f3fa;
+  border-radius: 0.6rem;
+  font-weight: 900;
+  color: ${({ theme }) => theme.primaryColor};
+  float: right;
+`;
