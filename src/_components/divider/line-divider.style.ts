5a54d1d20d891dcd76dde805f09ecde5fec395fe
wonza-hub <wonza8585@gmail.com>
2024-10-16 23:32:37 +0900

    refactor: 구분선 상하 마진 prop 추가

--- src/_components/divider/line-divider.style.ts
+++ src/_components/divider/line-divider.style.ts
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
-// props를 활용하여 height를 동적으로 설정
-export const LineDivider = styled.div<{ height?: string }>`
+export const LineDivider = styled.div<{
+  height?: string;
+  verticalMargin?: string;
+}>`
   width: 100%;
   height: ${(props) => props.height || "0.5px"}; // height가 없으면 기본값 1px
   background-color: #ebf0fa;
-  margin: 0.6rem 0;
+  margin: ${(props) => props.verticalMargin || "0.6rem"} 0;
 `;
--- src/_components/divider/line-divider.tsx
+++ src/_components/divider/line-divider.tsx
@@ -3,7 +3,11 @@ import * as S from "./line-divider.style";
 // COMPONENT: 라인 구분자
 type LineDividerProps = {
   height?: string;
+  verticalMargin?: string;
 };
-export default function LineDivider({ height }: LineDividerProps) {
-  return <S.LineDivider height={height} />;
+export default function LineDivider({
+  height,
+  verticalMargin,
+}: LineDividerProps) {
+  return <S.LineDivider height={height} verticalMargin={verticalMargin} />;
 }
