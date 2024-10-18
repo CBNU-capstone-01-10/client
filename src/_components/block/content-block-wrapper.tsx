ee58b6ce35be7136e184fc0f7b28e6fb49c33ba6
wonza-hub <wonza8585@gmail.com>
2024-10-18 11:33:33 +0900

    refactor: 컨텐츠 박스 래퍼 인터페이스 명시

--- src/_components/block/content-block-wrapper.tsx
+++ src/_components/block/content-block-wrapper.tsx
@@ -1,5 +1,14 @@
+import { ReactNode } from "react";
 import * as S from "./content-block-wrapper.style";
 
-export default function ContentBlockWrapper({ children, height }) {
+// COMPONENT: 컨텐츠를 담는 기본 박스형 래퍼
+interface IContentBlockWrapperProps {
+  children: ReactNode;
+  height: string;
+}
+export default function ContentBlockWrapper({
+  children,
+  height,
+}: IContentBlockWrapperProps) {
   return <S.Wrapper height={height}>{children}</S.Wrapper>;
 }
