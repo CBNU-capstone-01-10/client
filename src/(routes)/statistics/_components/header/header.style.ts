01e9701466dcfef8ba5afa5e767a6dd394b3c704
wonza-hub <wonza8585@gmail.com>
2024-10-19 22:03:11 +0900

    design: 헤더 네비게이션 sticky로 수정

--- src/(routes)/statistics/_components/header/header.style.ts
+++ src/(routes)/statistics/_components/header/header.style.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  position: relative;
-  z-index: 100;
+  position: sticky;
+  top: 0;
 `;
 export const MyScoreWrapper = styled.div`
   height: fit-content;
