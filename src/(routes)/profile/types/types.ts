b8bf5782d0885ba605e00a80fc0740410ad54b88
wonza-hub <wonza8585@gmail.com>
2024-10-30 23:53:50 +0900

    feat: api 수정에 따른 타입 변경

--- src/(routes)/profile/types/types.ts
+++ src/(routes)/profile/types/types.ts
@@ -1,6 +1,10 @@
 // 사용자 개인정보
 export interface IPersonalInfo {
-  pfp: string | undefined;
+  pfp: {
+    curr: string;
+    is_default: boolean;
+    user_id: number;
+  };
   username: string;
   alias: string | null;
   email: string;
