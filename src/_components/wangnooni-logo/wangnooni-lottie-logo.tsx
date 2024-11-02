d99a988b17f93ed2deedfb25afc14668db6937f2
wonza-hub <wonza8585@gmail.com>
2024-11-02 20:40:09 +0900

    feat: 메인 로고 lottie 컴포넌트 추가

---
+++ src/_components/wangnooni-logo/wangnooni-lottie-logo.tsx
@@ -0,0 +1,6 @@
+import Lottie from "lottie-react";
+import wangnooniLottie from "../../assets/lottie/wangnooni-lottie.json";
+
+export default function WangnooniLottieLogo() {
+  return <Lottie animationData={wangnooniLottie} />;
+}
