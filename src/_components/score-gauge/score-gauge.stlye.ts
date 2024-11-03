d85f520e8e57813962c894224052aa1534cd3675
wonza-hub <wonza8585@gmail.com>
2024-11-03 22:33:47 +0900

    design: 점수 게이지바 디자인 수정

--- src/_components/score-gauge/score-gauge.stlye.ts
+++ src/_components/score-gauge/score-gauge.stlye.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { IMAGE_MAP } from "../../constants/constants";
+import { pulsateFwd } from "../../styles/global-style";
 
 export const Container = styled.div`
   display: flex;
@@ -12,25 +13,25 @@ export const Container = styled.div`
   align-items: center;
   box-shadow: rgba(13, 38, 76, 0.19) 0px 9px 20px;
 `;
+
 interface IScoreBarProps {
   $scoreStartValue: number;
   score: number;
 }
+
 export const ScoreBar = styled.div.attrs<IScoreBarProps>((props) => ({
   style: {
     background: `conic-gradient(
       from 0deg,
-      ${props.score >= 0 ? `rgba(85,91,255,0.7)` : `rgba(240, 46, 170,0.7)`}
-      ,
-      ${props.score >= 0 ? `rgba(45,53,255,0.7)` : `rgba(240, 46, 170,0.8)`}
-      ,
+      ${props.score >= 0 ? `rgba(85,91,255,0.7)` : `rgba(240, 46, 170,0.7)`},
+      ${props.score >= 0 ? `rgba(45,53,255,0.7)` : `rgba(240, 46, 170,0.8)`},
       ${props.score >= 0 ? `rgba(0,9,255,0.7)` : `rgba(240, 46, 170,0.9)`}
     )`,
   },
 }))<IScoreBarProps>`
   position: relative;
-  width: 19rem;
-  height: 19rem;
+  width: 18.9rem;
+  height: 18.9rem;
   border-radius: 50%;
   transition: background 0.1s linear;
   display: flex;
@@ -43,10 +44,11 @@ export const ScoreBar = styled.div.attrs<IScoreBarProps>((props) => ({
     width: 19rem;
     height: 19rem;
     border-radius: 50%;
-    background: conic-gradient(
-      transparent ${(props) => props.$scoreStartValue * 3.6}deg,
-      #ffffff 0deg
-    );
+    background: ${(props) =>
+      `conic-gradient(
+        transparent ${props.$scoreStartValue * 3.6}deg,
+        rgba(199, 200, 204, 0.8) 0deg
+      )`};
   }
 `;
 
@@ -56,26 +58,42 @@ export const ScoreLabelWrapper = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   border-radius: 50%;
   transform: translate(-50%, -50%);
   background-color: white;
   z-index: 100;
 `;
+
 interface IScoreLabelProps {
   label: string;
   score: number;
 }
-export const ScoreLabel = styled.img.attrs<IScoreLabelProps>((props) => ({
+
+export const ScoreLabelImage = styled.img.attrs<IScoreLabelProps>((props) => ({
   src: IMAGE_MAP[props.label],
 }))<IScoreLabelProps>`
-  position: absolute;
-  top: 45%;
-  left: 50%;
-  width: 70%;
-  height: 70%;
-  transform: translate(-50%, -50%);
+  width: 55%;
+  height: 55%;
+  margin-bottom: 3rem;
   filter: drop-shadow(
     0 -1rem 4rem ${(props) => (props.score > 0 ? `rgba(8, 112, 184, 0.7)` : `rgba(240, 46, 170, 0.7)`)}
   );
+  transform-origin: top left;
+  animation: ${pulsateFwd} 0.5s ease-in-out infinite both;
+  z-index: 100;
+`;
+
+export const ScoreLabelTitle = styled.h1`
+  position: absolute;
+  font-size: 1.4rem;
+  font-weight: 600;
+  letter-spacing: 0.1rem;
+  bottom: 2rem;
+  left: 50%;
+  transform: translate(-50%, -50%);
   z-index: 100;
 `;
