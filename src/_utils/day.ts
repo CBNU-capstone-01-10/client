ac7bd2e72cf76a8b8ac6e1bbb4fd5695be2ae100
wonza-hub <wonza8585@gmail.com>
2024-11-03 12:30:50 +0900

    feat: 요일 배열 상수 적용

--- src/_utils/day.ts
+++ src/_utils/day.ts
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import { WEEK_DAYS } from "../constants/constants";
 
 /**
  * 최근 일주일의 순차적인 요일들을 담은 배열을 생성하는 함수
@@ -7,12 +8,11 @@ import dayjs from "dayjs";
 export const getRecentSevenDays = () => {
   const today = dayjs();
 
-  const weekDays = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
   const recentWeekLabels = Array(7)
     .fill(0)
     .map(
       (_, idx) =>
-        weekDays[
+        WEEK_DAYS[
           dayjs(today)
             .subtract(6 - idx, "day")
             .day()
