907c17dd8e27b76059113decdd77e6820dbe65b4
wonza-hub <wonza8585@gmail.com>
2024-11-03 12:36:31 +0900

    fix: api 변경에 따른 수정

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios, { AxiosError, AxiosResponse } from "axios";
 import isServerError from "../error/is-server-error";
 import { IPersonalInfo } from "../(routes)/profile/types/types";
@@ -8,9 +8,9 @@ export const useGetPersonalInfo = () => {
   return useQuery<AxiosResponse<IPersonalInfo>, AxiosError, IPersonalInfo>({
     queryKey: ["self-info"],
     queryFn: async () => {
-      const myProfileURL = `/api/self`;
+      const myProfileURL = `/api/users/self`;
       return await axios.get(myProfileURL).then((res) => {
-        return res.data.response;
+        return res.data;
       });
     },
     retry: 0,
@@ -18,18 +18,22 @@ export const useGetPersonalInfo = () => {
 };
 
 // PUT: 사용자 개인정보
-export const useEditPersonalInfo = () => {
-  //   const queryClient = useQueryClient();
+export const useEditPersonalInfo = (userId: number) => {
+  const queryClient = useQueryClient();
   return useMutation<AxiosResponse, AxiosError, FormData>({
     mutationFn: async (newProfileData) => {
-      const profileEditURL = `/api/self`;
+      const profileEditURL = `/api/users/${userId}`;
       return await axios.put(profileEditURL, newProfileData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
+      }).catch((err)=>console.log(err));
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["self-info"],
       });
     },
-    onSuccess: (res) => {},
     onError: (err) => {
       if (!isServerError(err)) {
         alert("에러가 발생했습니다. 관리자에게 문의해주세요.");
