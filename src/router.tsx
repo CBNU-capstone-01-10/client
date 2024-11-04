250b873d823f9d80551c515692d8a5ef790a98c3
wonza-hub <wonza8585@gmail.com>
2024-11-04 17:07:48 +0900

    rename: 메인 레이아웃 파일명 수정

--- src/router.tsx
+++ src/router.tsx
@@ -2,9 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import NotFoundErrorPage from "./(routes)/_errors/_components/NotFoundErrorPage";
 import SignupPage from "./(routes)/(account)/signup/signup-page";
 import SigninPage from "./(routes)/(account)/signin/page";
-import MainLayout from "./(routes)/MainLayout";
+import MainLayout from "./(routes)/main-layout";
 import SignupLayout from "./(routes)/signup-layout";
-import SplashLayout from "./(routes)/SplashLayout";
 
 const router = createBrowserRouter([
   {
