c22ab73b5b28e2d857dfe71af407cb4a8507d7f1
wonza-hub <wonza8585@gmail.com>
2024-11-05 02:55:07 +0900

    feat: actions 정보 없을 때 점수 게이지의 기본표시 추가

--- src/_components/score-gauge/score-gauge.tsx
+++ src/_components/score-gauge/score-gauge.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import * as S from "./score-gauge.stlye";
 import { useDriverActionsStore } from "../../store/use-driver-actions";
 import { ACTION_LABEL, ActionLabel } from "../../constants/constants";
+import WangnooniLottieLogo from "../wangnooni-logo/wangnooni-lottie-logo";
 
 export default function ScoreGauge() {
   const { driverActions } = useDriverActionsStore();
@@ -28,24 +29,31 @@ export default function ScoreGauge() {
 
   return (
     <S.Container>
-      <S.ScoreBar
-        $scoreStartValue={displayedScore}
-        score={driverActions[0]?.score}
-      >
-        {/* 라벨 표시 */}
-        <S.ScoreLabelWrapper>
-          <S.ScoreLabelImage
-            label={driverActions[0]?.label}
-            score={driverActions[0]?.score}
-            safeDriving={driverActions[0]?.safe_driving}
-          />
-          <S.ScoreLabelTitle>
-            {driverActions[0]?.safe_driving
-              ? "안전운전"
-              : ACTION_LABEL[driverActions[0]?.label as ActionLabel]}
-          </S.ScoreLabelTitle>
-        </S.ScoreLabelWrapper>
-      </S.ScoreBar>
+      {/* 라벨 표시 */}
+      {driverActions.length === 0 ? (
+        <>
+          <WangnooniLottieLogo height="12rem" />
+          <S.DefaultTitle>오늘 하루도 안전하게 운전하세요!</S.DefaultTitle>
+        </>
+      ) : (
+        <S.ScoreBar
+          $scoreStartValue={displayedScore}
+          score={driverActions[0]?.score}
+        >
+          <S.ScoreLabelWrapper>
+            <S.ScoreLabelImage
+              label={driverActions[0]?.label}
+              score={driverActions[0]?.score}
+              safeDriving={driverActions[0]?.safe_driving}
+            />
+            <S.ScoreLabelTitle>
+              {driverActions[0]?.safe_driving
+                ? "안전운전"
+                : ACTION_LABEL[driverActions[0]?.label as ActionLabel]}
+            </S.ScoreLabelTitle>
+          </S.ScoreLabelWrapper>
+        </S.ScoreBar>
+      )}
     </S.Container>
   );
 }
