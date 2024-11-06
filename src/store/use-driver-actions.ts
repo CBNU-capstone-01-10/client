69cc7cf62aa89c832f367ee384bf99064bdb786d
wonza-hub <wonza8585@gmail.com>
2024-11-06 14:47:19 +0900

    feat: action 전역 상태 업데이트 로직 수정 새로운 객체를 그대로 추가하는 방법에서 기존 객체와 label이 같으면 점수만 변경하게 수정

--- src/store/use-driver-actions.ts
+++ src/store/use-driver-actions.ts
@@ -1,8 +1,8 @@
+// STORE: 운전자 행위 관련 전역 상태 스토어
 import { create } from "zustand";
 import { IDriverActionResponse } from "../(routes)/record/types/type";
 import { MAX_LIVE_SCORE_LOG_SIZE } from "../constants/constants";
 
-// STORE: 운전자 행위 관련 전역 상태 스토어
 interface IDriverActionsState {
   driverActions: IDriverActionResponse[];
   addDriverAction: (driverAction: IDriverActionResponse) => void;
@@ -11,30 +11,28 @@ export const useDriverActionsStore = create<IDriverActionsState>((set) => ({
   driverActions: [],
 
   // 새로 받은 score 추가
-  addDriverAction: (newDriverAction: IDriverActionResponse) =>
+  addDriverAction: (newDriverAction: IDriverActionResponse) => {
     set((state) => {
       const recentAction = state.driverActions[0]; // 가장 최근 데이터를 가져옴
 
-      // 가장 최근 데이터와 newDriverAction의 label이 같은지 확인
-      if (recentAction && newDriverAction.label === recentAction.label) {
-        // label이 같은 경우: recentAction의 score에 newDriverAction의 score를 더함
-        // 기존 배열에서 가장 최근 데이터를 업데이트된 score로 변경
-        const updatedRecentAction = {
-          ...recentAction,
-          score: recentAction.score + newDriverAction.score,
-        };
-
-        return {
-          driverActions: [updatedRecentAction, ...state.driverActions.slice(1)],
-        };
-      } else {
+      // 배열이 비어있거나 label이 다를 경우 새로운 action 추가
+      if (!recentAction || recentAction.label !== newDriverAction.label) {
         // 큐의 최대 크기를 초과하면 마지막 요소 제거
-        if (state.driverActions.length === MAX_LIVE_SCORE_LOG_SIZE) {
-          state.driverActions.pop();
-        }
+        const updatedDriverActions =
+          state.driverActions.length === MAX_LIVE_SCORE_LOG_SIZE
+            ? state.driverActions.slice(0, -1)
+            : state.driverActions;
 
-        // label이 다르면 newDriverAction을 기존 로그에 추가
-        return { driverActions: [newDriverAction, ...state.driverActions] };
+        return { driverActions: [newDriverAction, ...updatedDriverActions] };
+      } else {
+        // label이 동일한 경우 최근 action의 score만 업데이트
+        return {
+          driverActions: [
+            { ...recentAction, score: newDriverAction.score },
+            ...state.driverActions.slice(1),
+          ],
+        };
       }
-    }),
+    });
+  },
 }));
