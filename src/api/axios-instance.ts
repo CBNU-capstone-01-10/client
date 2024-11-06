4b17f79e6a3b5d27b735237b5b9dd770c3614416
wonza-hub <wonza8585@gmail.com>
2024-11-06 14:44:19 +0900

    refactor: axios-interceptor 객체 추가 및 응답 처리 로직 추가

---
+++ src/api/axios-instance.ts
@@ -0,0 +1,51 @@
+// INTERCEPTOR: Axios 요청, 응답을 가로채어 중간처리를 하는 객체
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { IServerErrorResponse } from "../interface/error-interface";
+import isServerError from "../error/is-server-error";
+
+export const axiosInstance = axios.create({
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+// 응답 처리
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  (error) => {
+    if (!isServerError(error)) {
+      console.error("알 수 없는 에러가 발생했습니다. 관리자에게 문의해주세요.");
+      return;
+    }
+
+    // error는 AxiosError<IServerErrorResponse> 타입
+    const { status, data } =
+      error.response as AxiosResponse<IServerErrorResponse>;
+
+    if (error.response) {
+      switch (status) {
+        case 400:
+          // 400:
+          break;
+        case 401:
+          // 401: UNAUTHORIZED
+          if (data.message === "Unauthorized") {
+            window.location.href = "/";
+          }
+          break;
+        case 403:
+          // 403:
+          break;
+        case 404:
+          // 404:
+          break;
+        case 500:
+          // 500:
+          break;
+        default:
+          // 기타 상태 코드 처리 로직
+          break;
+      }
+    }
+    return Promise.reject(error);
+  }
+);
