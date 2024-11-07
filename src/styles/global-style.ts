12c2bb09bdd9886381369a19ab433fe9af0f3b60
wonza-hub <wonza8585@gmail.com>
2024-11-07 19:57:23 +0900

    design: 왼쪽에서부터 뒤집히며 등장하는 keyframes 추가

--- src/styles/global-style.ts
+++ src/styles/global-style.ts
@@ -5,7 +5,8 @@ import reset from "styled-reset";
 const TOP_NAVBAR_HEIGTH = "6rem";
 const BTM_NAVBAR_HEIGTH = "5.5rem";
 
-// KEYFRAMES: 펄스 효과
+// KEYFRAMES
+// 펄스 효과
 export const pulsateFwd = keyframes`
   0% {
     transform: scale(1);
@@ -15,6 +16,23 @@ export const pulsateFwd = keyframes`
   }
   100% {
     transform: scale(1);
+  }
+`;
+// 왼쪽에서부터 뒤집히며 등장하는 효과
+export const swingInLeftFwd = keyframes`
+  0% {
+    -webkit-transform: rotateY(100deg);
+            transform: rotateY(100deg);
+    -webkit-transform-origin: left;
+            transform-origin: left;
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: rotateY(0);
+            transform: rotateY(0);
+    -webkit-transform-origin: left;
+            transform-origin: left;
+    opacity: 1;
   }
 `;
 
