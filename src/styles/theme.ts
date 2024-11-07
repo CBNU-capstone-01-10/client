ebf99c8cdf8d142f8e2f9f15e44f6ffa4f72283c
wonza-hub <wonza8585@gmail.com>
2024-11-08 00:44:00 +0900

    design: 경고 주요 배경색 rgb값 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,7 @@ export const theme = {
     primaryColor: `rgba(31, 135, 254)`,
     secondaryColor: `rgba(47, 60, 120)`,
     primaryBgColor: `rgba(244, 246, 250)`,
-    primaryCautionBgColor: `rgba(128, 0, 0)`,
+    primaryCautionBgColor: `rgba(255, 15, 15, 1)`,
     transparentWhite: `rgba(255, 255, 255, 0.3)`,
   },
 };
