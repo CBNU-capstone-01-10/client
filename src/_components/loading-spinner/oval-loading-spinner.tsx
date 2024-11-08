e2d6ee0ae810cb617cd08a7ba9425533152f2dbc
wonza-hub <wonza8585@gmail.com>
2024-11-09 00:34:57 +0900

    design: 로딩스피너 래퍼 추가

--- src/_components/loading-spinner/oval-loading-spinner.tsx
+++ src/_components/loading-spinner/oval-loading-spinner.tsx
@@ -1,17 +1,27 @@
 // COMPONENT: 로딩스피너
 import { Oval } from "react-loader-spinner";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  width: 100%;
+  height: 7rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 export default function OvalLoadingSpinner() {
   return (
-    <Oval
-      visible={true}
-      height="40"
-      width="40"
-      color="blue"
-      secondaryColor="blue"
-      ariaLabel="oval-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-    />
+    <Wrapper>
+      <Oval
+        visible={true}
+        height="40"
+        width="40"
+        color="blue"
+        secondaryColor="blue"
+        ariaLabel="oval-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </Wrapper>
   );
 }
