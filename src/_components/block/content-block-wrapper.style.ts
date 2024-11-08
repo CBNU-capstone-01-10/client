7b9a832110c14c7bf570c93da011edda78f0f155
wonza-hub <wonza8585@gmail.com>
2024-11-08 19:29:15 +0900

    refactor: 컨텐츠 블럭 래퍼 padding 커스텀 가능하게 수정

--- src/_components/block/content-block-wrapper.style.ts
+++ src/_components/block/content-block-wrapper.style.ts
@@ -2,12 +2,14 @@ import styled from "styled-components";
 interface WrapperProps {
   height?: string;
   margin?: string;
+  padding?: string;
 }
 
 export const Wrapper = styled.section<WrapperProps>`
   width: 100%;
   height: ${(props) => props.height || "5rem"};
   margin: ${(props) => props.margin || "0 0 1.2rem 0"};
+  padding: ${(props) => props.padding || "0"};
   background-color: white;
   border-radius: 1.5rem;
   box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;
--- src/_components/block/content-block-wrapper.tsx
+++ src/_components/block/content-block-wrapper.tsx
@@ -4,16 +4,18 @@ import * as S from "./content-block-wrapper.style";
 // COMPONENT: 컨텐츠를 담는 기본 박스형 래퍼
 interface IContentBlockWrapperProps {
   children: ReactNode;
-  height: string;
-  margin: string;
+  height?: string;
+  margin?: string;
+  padding?: string;
 }
 export default function ContentBlockWrapper({
   children,
   height,
   margin,
+  padding,
 }: IContentBlockWrapperProps) {
   return (
-    <S.Wrapper height={height} margin={margin}>
+    <S.Wrapper height={height} margin={margin} padding={padding}>
       {children}
     </S.Wrapper>
   );
