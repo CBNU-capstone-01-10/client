ba8f142d248edb6384a61495457c006cc4c05cff
wonza-hub <wonza8585@gmail.com>
2024-11-08 23:06:56 +0900

    feat: 통계 페이지 내 위험 운전 분석 컴포넌트 추가

--- src/(routes)/statistics/page.tsx
+++ src/(routes)/statistics/page.tsx
@@ -2,6 +2,7 @@ import Header from "./_components/header/header";
 import RecentHistory from "./_components/recent-history/recent-history";
 import * as S from "./page.style";
 import RecentSevenDaysScore from "./_components/recent-seven-days-score/recent-seven-days-score";
+import RecentSevenDaysBadActionsGraph from "./_components/recent-seven-days-bad-actions-graph/recent-seven-days-bad-actions-graph";
 
 // PAGE: 안전점수 관련 통계 페이지
 export default function Page() {
@@ -10,6 +11,7 @@ export default function Page() {
       <S.ContentWrapper>
         <Header />
         <RecentSevenDaysScore />
+        <RecentSevenDaysBadActionsGraph />
         <RecentHistory />
       </S.ContentWrapper>
     </S.PageWrapper>
