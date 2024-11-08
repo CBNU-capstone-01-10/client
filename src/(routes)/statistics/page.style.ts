2ee9328f6df75c4b92c419090356843a6cb25899
wonza-hub <wonza8585@gmail.com>
2024-11-08 23:07:10 +0900

    design: 통계 페이지 디자인 수정

--- src/(routes)/statistics/page.style.ts
+++ src/(routes)/statistics/page.style.ts
@@ -5,8 +5,10 @@ export const PageWrapper = styled.div`
   height: 100vh;
   padding: var(--top-navbar-height) 1rem 0 1rem;
   background-color: ${({ theme }) => theme.primaryBgColor};
+  overflow: auto;
 `;
 export const ContentWrapper = styled.div`
   width: 100%;
-  height: 100%;
+  height: fit-content;
+  padding-bottom: calc(var(--btm-navbar-height) + 3rem);
 `;
