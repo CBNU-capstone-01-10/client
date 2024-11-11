9913ddd3f4764776e206d90f31d575a14b6acf2d
wonza-hub <wonza8585@gmail.com>
2024-11-11 21:04:55 +0900

    design: 회원가입 페이지 홈버튼 안보이는 문제 수정

--- src/(routes)/signup-layout.tsx
+++ src/(routes)/signup-layout.tsx
@@ -3,10 +3,11 @@ import TopNavBar from "../_components/topNavBar/top-navbar";
 import styled from "styled-components";
 import NavIcon from "../_components/icon/navigation-icon";
 import { MdHomeFilled } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const HomeBtn = styled.div`
   position: absolute;
-  top: 3.6rem;
+  top: 3rem;
   left: 2.8rem;
   width: 2rem;
   height: 2rem;
@@ -18,9 +19,11 @@ const HomeBtn = styled.div`
 export default function SignupLayout() {
   return (
     <>
-      <TopNavBar title={"회원가입"} height={"7rem"} />
+      <TopNavBar title={"회원가입"} height={"10rem"} />
       <HomeBtn>
-        <NavIcon icon={MdHomeFilled} size="1.6rem" color="white" to="/" />
+        <Link to={"/"}>
+          <NavIcon icon={MdHomeFilled} size="1.6rem" color="grey" />
+        </Link>
       </HomeBtn>
       <Outlet />
     </>
