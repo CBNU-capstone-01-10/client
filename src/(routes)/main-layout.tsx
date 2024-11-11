7ad8a82e0c2220d087e7cb3f0f2052f81db5de3c
wonza-hub <wonza8585@gmail.com>
2024-11-11 12:52:51 +0900

    fix: 사용자 아이디 전역 상태 저장시 id에서 pfp하위의 user_id로 수정

--- src/(routes)/main-layout.tsx
+++ src/(routes)/main-layout.tsx
@@ -29,7 +29,7 @@ export default function MainLayout() {
   const { keepUserId } = useUserStore();
 
   useEffect(() => {
-    keepUserId(userData?.id);
+    keepUserId(userData?.pfp.user_id);
   }, []);
 
   return (
