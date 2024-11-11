b06f1f01b14e11cd543a2311c87c8b5487d35cc0
wonza-hub <wonza8585@gmail.com>
2024-11-12 01:18:08 +0900

    chore: vite 환경변수 적용하여 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,35 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "localhost+1-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "localhost+1.pem")),
-    },
-    proxy: {
-      "/api": {
-        target: "https://localhost:8080",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: false,
-        ws: true,
+export default defineConfig(({ mode }) => {
+  // 환경변수 로드
+  const env = loadEnv(mode, process.cwd());
+
+  const sslCertName = env.VITE_SSL_CERT_NAME;
+  const proxyProtocol = env.VITE_API_PROTOCOL;
+  const proxyHost = env.VITE_API_HOST;
+  const proxyPort = env.VITE_API_PORT;
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      // 로컬 환경에서만 적용
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, `${sslCertName}-key.pem`)),
+        cert: fs.readFileSync(path.resolve(__dirname, `${sslCertName}.pem`)),
+      },
+      proxy: {
+        "/api": {
+          target: `${proxyProtocol}://${proxyHost}:${proxyPort}`,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+          secure: false,
+          ws: true,
+        },
       },
     },
-  },
+  };
 });
