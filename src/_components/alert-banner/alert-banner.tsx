e23da7676ef225125b1a4d97baf1e8ca371227e1
wonza-hub <wonza8585@gmail.com>
2024-11-13 02:04:51 +0900

    fix: 얼럿창 일정 시간 지난 후 사라지게 수정

--- src/_components/alert-banner/alert-banner.tsx
+++ src/_components/alert-banner/alert-banner.tsx
@@ -1,4 +1,3 @@
-// COMPONENT: 얼럿 메시지
 import { Alert } from "antd";
 import { AxiosError } from "axios";
 import { useState, useEffect } from "react";
@@ -12,7 +11,9 @@ interface IAlertProps {
   top?: string;
   left?: string;
   type?: "error" | "success" | "info" | "warning" | undefined;
+  duration?: number; // 얼럿이 사라질 시간 (밀리초 단위)
 }
+
 export default function AlertBanner({
   error,
   errorMessage,
@@ -20,18 +21,18 @@ export default function AlertBanner({
   top,
   left,
   type,
+  duration = 3000, // 기본값 3초
 }: IAlertProps) {
   const [message, setMessage] = useState<string | undefined>("");
+  const [visible, setVisible] = useState(true);
 
   useEffect(() => {
-    // error.response?.data가 있고, IServerErrorResponse 형식인 경우 메시지 설정
     if (
       error?.response?.data &&
       (error.response.data as IServerErrorResponse).message
     ) {
       setMessage((error.response.data as IServerErrorResponse).message);
     } else {
-      // 데이터가 비어있거나 형식이 다른 경우 기본 메시지 설정
       setMessage("잠시후 다시 시도해주세요.");
     }
   }, [error]);
@@ -41,6 +42,15 @@ export default function AlertBanner({
       setMessage(errorMessage);
     }
   }, [errorMessage]);
+
+  useEffect(() => {
+    // duration 시간 후에 visible 상태를 false로 변경하여 얼럿 창을 숨깁니다.
+    const timer = setTimeout(() => setVisible(false), duration);
+    return () => clearTimeout(timer); // 컴포넌트가 언마운트될 때 타이머 정리
+  }, [duration]);
+
+  // visible 상태가 false일 때 null을 반환하여 컴포넌트 숨기기
+  if (!visible) return null;
 
   return (
     <S.Wrapper width={width} top={top} left={left}>
