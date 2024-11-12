3128e8f92115a57c26a1a3df3811b901643a22e6
wonza-hub <wonza8585@gmail.com>
2024-11-13 03:10:13 +0900

    design: 알림창 전역 색상 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,6 +4,7 @@ export const theme = {
     secondaryColor: `rgba(47, 60, 120)`,
     primaryBgColor: `rgba(244, 246, 250)`,
     primaryCautionBgColor: `rgba(255, 15, 15, 1)`,
+    transparentCautionBgColor: `rgba(255, 15, 15, 0.9)`,
     transparentWhite: `rgba(255, 255, 255, 0.3)`,
   },
 };
