328cc53c1d9160242bda7bdbc53175f47915656e
wonza-hub <wonza8585@gmail.com>
2025-01-17 17:35:35 +0900

    refactor: 목업 함수 모아서 한 번에 내보내는 index 추가

---
+++ mock/index.ts
@@ -0,0 +1,10 @@
+import { setupMockForRegister } from "./account";
+import { setupMockForActions } from "./action";
+import { setupMockForUser } from "./user";
+
+const setupMocks = {
+  setupMockForRegister,
+  setupMockForActions,
+  setupMockForUser,
+};
+export default setupMocks;
--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,22 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import router from "./router";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
-import { setupMockForRegister } from "../mock/account";
-import { setupMockForActions } from "../mock/action";
-import { setupMockForUser } from "../mock/user";
+import { axiosInstance } from "./api/axios-instance";
+import setupMocks from "../mock/index";
 
-// // 개발용: MOCKUP
-// const mock = new MockAdapter(axios);
-// // 환경 변수 기반 목업 활성화
-// if (import.meta.env.VITE_ENABLE_MOCK === "true") {
-//   // 목업 설정 활성화
-//   setupMockForRegister(mock);
-//   setupMockForActions(mock);
-//   setupMockForUser(mock);
-//   console.log(
-//     "Axios Mock Adapter가 활성화되어 axios 요청을 가로챌 준비가 되었습니다."
-//   );
-// } else {
-//   mock.restore();
-//   console.log("Axios Mock Adapter가 비활성화 되었습니다.");
-// }
+// 개발용: MOCKUP
+const mock = new MockAdapter(axiosInstance);
+// 환경 변수 기반 목업 활성화
+if (import.meta.env.VITE_ENABLE_MOCK === "true") {
+  // 목업 설정 활성화
+  Object.values(setupMocks).forEach((setupMock) => setupMock(mock));
+  console.log(
+    "Axios Mock Adapter가 활성화되어 axios 요청을 가로챌 준비가 되었습니다."
+  );
+} else {
+  mock.restore();
+  console.log("Axios Mock Adapter가 비활성화 되었습니다.");
+}
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -37,7 +34,7 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      {/* <ReactQueryDevtools /> */}
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
