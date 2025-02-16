b570ab7a5c9530de605e15da1dc9e40879b0d2bb
wonza-hub <wonza8585@gmail.com>
2025-02-16 15:31:30 +0900

    refactor: axios 인스턴스 설정에 credential 속성 추가

--- src/api/axios-instance.ts
+++ src/api/axios-instance.ts
@@ -7,6 +7,7 @@ export const axiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 // 응답 처리
 axiosInstance.interceptors.response.use(
