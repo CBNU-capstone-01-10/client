60da5ea269d6668eeba1bd51f65fe80c5f95d6ce
wonza-hub <wonza8585@gmail.com>
2025-02-17 20:20:03 +0900

    refactor: 회원가입 진행중 앱 내 라우팅/브라우저 뒤로가기를 통한 사용자 이탈 시 예외 처리 추가

--- src/(routes)/(account)/signup/signup-page.tsx
+++ src/(routes)/(account)/signup/signup-page.tsx
@@ -1,8 +1,10 @@
 // PAGE: 회원가입 페이지(퍼널 형식)
+import { useNavigationType } from "react-router";
 import GenericForm from "../../../_components/form/generic-form";
 import { useFunnel } from "../../../hooks/useFunnel";
 import AccountSetup from "./components/account-setup";
 import FormSessionStorage from "./components/form-session-storage";
+import { useRef } from "react";
 
 const steps = [
   "이름 설정",
@@ -13,7 +15,20 @@ const steps = [
 ];
 
 export default function SignupPage() {
-  const { Funnel, Step, setStep } = useFunnel(steps[0]);
+  const { Funnel, Step, setStep, currentStep } = useFunnel(steps[0]);
+  const navigationType = useNavigationType(); // "POP" | "PUSH" | "REPLACE" 중 하나를 반환
+  const signupDefaultValuesRef = useRef({});
+
+  // EXCEPTION: 회원가입의 첫 번째 단계이고, 히스토리 스택의 타입이 PUSH인 경우는 새로운 계정 생성 시도로 판정
+  if (currentStep === steps[0] && navigationType === "PUSH") {
+    // 세션 스토리지 키-값 삭제
+    signupDefaultValuesRef.current = {};
+    sessionStorage.removeItem("accountSetup");
+  } else {
+    // 세션 스토리지에 저장된 폼 입력값 복원하기
+    const savedData = sessionStorage.getItem("accountSetup");
+    signupDefaultValuesRef.current = savedData ? JSON.parse(savedData) : {};
+  }
 
   const nextClickHandler = (step: string) => {
     setStep(step);
@@ -22,12 +37,13 @@ export default function SignupPage() {
     setStep(step);
   };
 
-  // STORAGE: 세션 스토리지에 저장된 폼 입력값 복원하기
-  const savedData = sessionStorage.getItem("accountSetup");
-  const defaultValues = savedData ? JSON.parse(savedData) : {};
-
   return (
-    <GenericForm formOptions={{ mode: "onChange", defaultValues }}>
+    <GenericForm
+      formOptions={{
+        mode: "onChange",
+        defaultValues: signupDefaultValuesRef.current,
+      }}
+    >
       <FormSessionStorage />
       <AccountSetup
         steps={steps}
