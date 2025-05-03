d9b21b870568c53a0d69652bbe140ca1aabaa8d0
wonza-hub <wonza8585@gmail.com>
2025-05-03 19:18:13 +0900

    fix: 회원가입 도중 이탈 후 url로 접근 시 처리 추가

--- src/(routes)/(account)/signup/components/form-session-storage.tsx
+++ src/(routes)/(account)/signup/components/form-session-storage.tsx
@@ -1,24 +1,16 @@
 // COMPONENT: 입력 폼에서 세션 스토리지(웹 스토리지 API)를 활용해 정보를 저장하거나 가져온다.
 // 상위 컴포넌트에 react hook form의 FormProvider가 존재해야 한다.
-import { useEffect } from "react";
 import { useFormContext } from "react-hook-form";
+import { useBeforeUnload } from "react-router-dom";
 
 export default function FormSessionStorage() {
   const { getValues } = useFormContext();
 
-  useEffect(() => {
+  useBeforeUnload(() => {
     // HANDLER: beforeunload 이벤트 발생 시, 세션 스토리지에 폼 입력값 데이터 저장
-    const handleBeforeUnload = () => {
-      const formData = getValues();
-      sessionStorage.setItem("accountSetup", JSON.stringify(formData));
-    };
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, [getValues]);
+    const formData = getValues();
+    sessionStorage.setItem("accountSetup", JSON.stringify(formData));
+  });
 
   return null;
 }
--- src/(routes)/(account)/signup/signup-page.tsx
+++ src/(routes)/(account)/signup/signup-page.tsx
@@ -16,19 +16,32 @@ const steps = [
 
 export default function SignupPage() {
   const { Funnel, Step, setStep, currentStep } = useFunnel(steps[0]);
-  const navigationType = useNavigationType(); // "POP" | "PUSH" | "REPLACE" 중 하나를 반환
   const signupDefaultValuesRef = useRef({});
 
+  // react-router-dom v6.4+에서 제공하는 useNavigationType 훅을 사용하여 현재 네비게이션 타입을 확인
+  const navigationType = useNavigationType(); // "POP" | "PUSH" | "REPLACE" 중 하나를 반환
+  // performance API 로부터 navigation entries 읽기
+  const perfEntries = performance.getEntriesByType(
+    "navigation"
+  ) as PerformanceNavigationTiming[];
+  const perfNavType = perfEntries.length > 0 ? perfEntries[0].type : null;
+
+  // 신규 가입 시도 여부 판단
+  const isNewSignup =
+    currentStep === steps[0] &&
+    (navigationType === "PUSH" || perfNavType === "navigate");
+
   // EXCEPTION: 회원가입의 첫 번째 단계이고, 히스토리 스택의 타입이 PUSH인 경우는 새로운 계정 생성 시도로 판정
-  if (currentStep === steps[0] && navigationType === "PUSH") {
+  if (isNewSignup) {
+    console.log("회원가입 페이지로 이동했습니다.");
     // 세션 스토리지 키-값 삭제
     signupDefaultValuesRef.current = {};
-    sessionStorage.removeItem("accountSetup");
   } else {
-    // 세션 스토리지에 저장된 폼 입력값 복원하기
+    // 세션 스토리지에 저장된 폼 입력값 복원하고, 세션 스토리지에서 삭제
     const savedData = sessionStorage.getItem("accountSetup");
     signupDefaultValuesRef.current = savedData ? JSON.parse(savedData) : {};
   }
+  sessionStorage.removeItem("accountSetup");
 
   const nextClickHandler = (step: string) => {
     setStep(step);
