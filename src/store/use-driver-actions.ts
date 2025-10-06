b2d562edce03cef063c6533a49af20b8aec47bfd
wonza-hub <wonza8585@gmail.com>
2025-10-06 16:44:30 +0900

    refactor: 운전자 행위 관련 전역 상태 스토어 수정 데이터 전송 조건부 로직을 api 호출부에서 처리함

--- src/store/use-driver-actions.ts
+++ src/store/use-driver-actions.ts
@@ -1,8 +1,10 @@
-// STORE: 운전자 행위 관련 전역 상태 스토어
 import { create } from "zustand";
 import { IDriverActionResponse } from "../(routes)/record/types/type";
 import { MAX_LIVE_SCORE_LOG_SIZE } from "../constants/constants";
 
+/**
+ * STORE: 운전자 행위 관련
+ */
 interface IDriverActionsState {
   driverActions: IDriverActionResponse["action"][];
   addDriverAction: (driverAction: IDriverActionResponse["action"]) => void;
@@ -11,29 +13,16 @@ interface IDriverActionsState {
 export const useDriverActionsStore = create<IDriverActionsState>((set) => ({
   driverActions: [],
 
-  // 새로 받은 score 추가
+  // 새로 받은 action 추가 (조건부 로직은 API 레이어에서 처리됨)
   addDriverAction: (newDriverAction: IDriverActionResponse["action"]) => {
     set((state) => {
-      const recentAction = state.driverActions[0]; // 가장 최근 데이터를 가져옴
+      // 큐의 최대 크기를 초과하면 마지막 요소 제거
+      const updatedDriverActions =
+        state.driverActions.length === MAX_LIVE_SCORE_LOG_SIZE
+          ? state.driverActions.slice(0, -1)
+          : state.driverActions;
 
-      // 배열이 비어있거나 label이 다를 경우 새로운 action 추가
-      if (!recentAction || recentAction.label !== newDriverAction.label) {
-        // 큐의 최대 크기를 초과하면 마지막 요소 제거
-        const updatedDriverActions =
-          state.driverActions.length === MAX_LIVE_SCORE_LOG_SIZE
-            ? state.driverActions.slice(0, -1)
-            : state.driverActions;
-
-        return { driverActions: [newDriverAction, ...updatedDriverActions] };
-      } else {
-        // label이 동일한 경우 최근 action의 score만 업데이트
-        return {
-          driverActions: [
-            { ...recentAction, score: newDriverAction.score },
-            ...state.driverActions.slice(1),
-          ],
-        };
-      }
+      return { driverActions: [newDriverAction, ...updatedDriverActions] };
     });
   },
   // 상태 초기화 함수
